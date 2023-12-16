0b59b07d1401eb3e0fa6c87696d7bd77c0ba45eb
SonyAkb <145361459+SonyAkb@users.noreply.github.com>
2023-12-16 12:31:50 +0300

    Метод половинного деления

---
+++ half division.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(double x) {
+	return x - 2 + sin(1 / x);
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	double E = pow(10, -6), a = 1, b = 2, c, iter = 0;
+	cout << "Уравнение:   x - 2 + sin(1 / x) = 0" << endl;
+	do {
+		c = (a + b) / 2;
+		if (f(a) * f(c) > 0) a = c;
+		else b = c;
+		++iter;
+	} while (abs(a - b) > E);
+
+	cout << "x = " << c << endl << "Количество итераций: " << iter;
+	return 0;
+}
